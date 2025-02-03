6b4607415602e05596461522eb94822795b64812
James202017 <56560143+James202017@users.noreply.github.com>
2025-02-04 02:01:11 +0500

    Create mtda.py

---
+++ mtda.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('7216405294:AAG1i86SbGAvtFvr-PJvnxjxsjalUpWd6EI')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types. Message) :
+markup = types. ReplyKeyboardMarkup ()
+markup.add(types.KeyboardButton('Открыть веб страницу', web_app=https://google.com) )
+await massage.answer('Добро пожаловать на этот сайт!', reply_markup=marcup)
+
+
+executor.start_polling(dp)
