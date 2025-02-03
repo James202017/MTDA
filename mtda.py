b0cd883119dd0cb94b20b16b153d425e7ace2e1b
James202017 <56560143+James202017@users.noreply.github.com>
2025-02-04 02:11:43 +0500

    Update mtda.py

--- mtda.py
+++ mtda.py
@@ -7,7 +7,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types. Message) :
     markup = types.ReplyKeyboardMarkup ()
-    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=https://google.com) )
+    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo (https://google.com)))
     await massage.answer('Добро пожаловать на этот сайт!', reply_markup=marcup)
 
 
